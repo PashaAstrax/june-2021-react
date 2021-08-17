146308219e58002593722cd6b7a8696a0293fef4
PashaAstrax <pashaastrax@gmail.com>
2021-08-17 12:50:16 +0300

    some change in User.js

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,13 +1,9 @@
 export default function User({thisUser, postsFunction}) {
-
-    const postsDisplay = () => {
-        postsFunction(thisUser.id)
-    }
-
     return (
         <div>
             <h3>{thisUser.id}. {thisUser.name} {""}
-            <button onClick={postsDisplay}>posts</button></h3>
+            <button onClick={() => postsFunction(thisUser.id)}>posts
+            </button></h3>
         </div>
     );
 }
