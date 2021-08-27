81f2037498df4530f74ba296392f7d2f4b687987
PashaAstrax <pashaastrax@gmail.com>
2021-08-27 13:28:21 +0300

    ClassWork #7 react in process...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import {useReducer} from "react";
+import reducer from "./reducers/reducer";
+
 export default function App() {
-  return (
+
+    let [{a, b}, dispatch] = useReducer(reducer,{a: 0, b: 0});
+    // let {a, b} = state;
+
+    return (
     <div>
-        App
+        <h1><i>state #1</i> - "{a}"</h1>
+        <button onClick={() => dispatch({obj: "1", diya: "+"})}>+</button>
+        <button onClick={() => dispatch({obj: "1", diya: "-"})}>-</button>
+        <h1><i>state #2</i> - "{b}"</h1>
+        <button onClick={() => dispatch({obj: "2", diya: "+"})}>+</button>
+        <button onClick={() => dispatch({obj: "2", diya: "-"})}>-</button>
     </div>
   );
 }
