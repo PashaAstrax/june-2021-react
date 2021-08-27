56816d2b5e59ee66b962a0c53c74d4dd8e2d4991
PashaAstrax <pashaastrax@gmail.com>
2021-08-27 15:13:40 +0300

    create ClassWork #7 react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import {useReducer} from "react";
+import reducer from "./reducers/reducer";
+
 export default function App() {
-  return (
-    <div>
-        App
+
+    let [{a, b, c}, dispatch] = useReducer(reducer,{a: 0, b: 0, c: 0});
+    // let {a, b, c} = state;
+
+    return (
+    <div className={"wrap"}>
+        <h1><i>state #1</i> - "{a}"</h1>
+        <button onClick={() => dispatch({obj: "1", diya: "+"})}>+</button>
+        <button onClick={() => dispatch({obj: "1", diya: "-"})}>-</button>
+        <h1><i>state #2</i> - "{b}"</h1>
+        <button onClick={() => dispatch({obj: "2", diya: "+"})}>+</button>
+        <button onClick={() => dispatch({obj: "2", diya: "-"})}>-</button>
+        <h1><i>state #3</i> - "{c}"</h1>
+        <button onClick={() => dispatch({obj: "3", diya: "+"})}>+</button>
+        <button onClick={() => dispatch({obj: "3", diya: "-"})}>-</button>
     </div>
   );
-}
+};
---
+++ src/reducers/reducer.js
@@ -0,0 +1,17 @@
+const reducer = (state, action) => {
+    if (action.obj === "1" && action.diya === "+") {
+        return {...state, a: state.a++}
+    } else if (action.obj === "1" && action.diya === "-") {
+        return {...state, a: state.a--}
+    } else if (action.obj === "2" && action.diya === "+") {
+        return {...state, b: state.b++}
+    } else if (action.obj === "2" && action.diya === "-") {
+        return {...state, b: state.b--}
+    } else if (action.obj === "3" && action.diya === "+") {
+        return {...state, c: state.c++}
+    } else if (action.obj === "3" && action.diya === "-") {
+        return {...state, c: state.c--}
+    }
+    return {...state};
+}
+export default reducer;
